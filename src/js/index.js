a54c3c21b536d199f4be333ddf404dda852e6dd0
Anastasia <nastyapralitch@yandex.by>
2023-10-25 15:26:00 +0300

    "added api and card img"

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,28 @@
-new Swiper(".swiper", {
-    //Стрелки
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+import './slider.js';
+
+// let url = 'https://65367a0fbb226bb85dd2306e.mockapi.io/:endpoint';
+
+fetch('https://65367a0fbb226bb85dd2306e.mockapi.io/wildberries')
+  .then(response => response.json())
+  .then(data => data.forEach(element => createCard(element.img)))
+ 
+
+
+  function createCard(imgSrc){
+ 
+  const catalogWrap = document.getElementsByClassName('catalog-wrapper')[0];
+  const cardItem = document.createElement('div');
+  cardItem.className = 'card-item';
+  const cardImg = document.createElement('div');
+  cardImg.className = 'card-img';
+  const img = document.createElement('img');
   
-    //Буллеты
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  
-    //Свайп
-    simulateTouch: true,
-    grabCursor: true,
-  
-    loop: true,
-  
-  });
+  img.src = imgSrc;
+
+cardImg.append(img);
+cardItem.append(cardImg);
+catalogWrap.append(cardItem)
+
+  }
+
+  console.log(1);
