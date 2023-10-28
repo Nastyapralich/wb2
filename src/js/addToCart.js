74edab87f75e67b27d2df5bf5729cfcd85f2da0f
Anastasia <nastyapralitch@yandex.by>
2023-10-28 15:40:32 +0300

    "delete all js"

--- src/js/addToCart.js
+++
@@ -1,9 +0,0 @@
-import { cards } from './index.js';
-
-export function addToCart(title, price, img, deliver) {
-  const item = { title, price, img, deliver };
-  cards.push(item);
-
-  localStorage.setItem('cards', JSON.stringify(cards));
-  // alert('Товар добавлен в корзину');
-}
--- src/js/generateSalePercent.js
+++
@@ -1,5 +0,0 @@
-export function generateSalePercent() {
-  let randomNumber = Math.random();
-  let result = Math.floor(randomNumber * (30 - 2 + 1)) + 2;
-  return result;
-}
